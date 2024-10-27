d4b1f3bee714d900037b5830db791b88d2ce2996
abdulsalamamtech <abdulsalamamtech@gmail.com>
2024-10-27 17:14:36 +0100

    Install laravel breeze starter kit

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
-
-
-// To issue a token
-Route::post('/tokens/create', function (Request $request) {
-    $token = $request->user()->createToken($request->token_name);
-
-    return ['token' => $token->plainTextToken];
 });
