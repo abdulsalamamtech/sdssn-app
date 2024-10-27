dee6a7f4092e7f3bef45d62655f4b6fa7307ea0e
abdulsalamamtech <abdulsalamamtech@gmail.com>
2024-10-27 16:44:06 +0100

    Modified boostrap app.php file for stateful api request

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+
+        //Incoming requests from your SPA can authenticate using Laravel's session cookies
+        $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
