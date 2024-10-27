abc62deaaa70583362e2a49cab07b08dc74a6e98
abdulsalamamtech <abdulsalamamtech@gmail.com>
2024-10-27 16:44:31 +0100

    Modified api route file

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+// To issue a token
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
