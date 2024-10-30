97d9109ed8482cbd691a59645f129d0bce592379
abdulsalamamtech <abdulsalamamtech@gmail.com>
2024-10-30 15:24:41 +0100

    Modified api route file

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/', function (Request $request) {
+    return [
+        'success' => true,
+        'message' => 'api is live',
+        'data' => [
+            'date' => now(),
+            'request' => $request->all(),
+            'environment' => config('app.env'),
+            'debug' => config('app.debug')
+        ],
+        'Laravel' => app()->version()
+    ];
+});
+
+
