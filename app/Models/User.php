c8ffdbb8553ab55b7e4bbcab11e8a41534dde61f
abdulsalamamtech <abdulsalamamtech@gmail.com>
2024-10-31 11:08:41 +0100

    Modified user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,19 +2,19 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Laravel\Sanctum\HasApiTokens;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasApiTokens;
 
     /**
-     * The attributes that are mass assignable.
+     * The attributes that are manormcore iceland tousled brook viral artisan.ss assignable.
      *
      * @var array<int, string>
      */
@@ -22,6 +22,18 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'first_name',
+        'last_name',
+        'security_question',
+        'answer',
+        'phone_number',
+        'gender',
+        'dob',
+        'address',
+        'state',
+        'membership_status',
+        'role',
+        'assigned_by',
     ];
 
     /**
@@ -44,6 +56,8 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
         ];
     }
 }
