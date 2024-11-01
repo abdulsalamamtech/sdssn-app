f368df865acb61a179ac76e1265834931a0a7561
abdulsalamamtech <abdulsalamamtech@gmail.com>
2024-11-01 12:29:48 +0100

    Modified api route

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,22 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
 
 
+// API test route
 Route::get('/', function (Request $request) {
     return [
         'success' => true,
         'message' => 'api is live',
         'data' => [
-            'date' => now(),
+            'date' => now()->format('Y-m-d H:i:s'),
             'request' => $request->all(),
             'environment' => config('app.env'),
             'debug' => config('app.debug')
@@ -22,4 +25,15 @@ Route::get('/', function (Request $request) {
     ];
 });
 
+
+// Authentication routes
+// Register route
+Route::post('register', [AuthController::class, 'register'])
+    ->middleware('guest');
+// Login route
+Route::post('login', [AuthController::class, 'login'])
+    ->middleware('guest');
+// Logout route
+Route::post('logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum');
 
