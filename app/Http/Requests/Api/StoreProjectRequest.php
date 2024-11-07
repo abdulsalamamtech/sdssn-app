59781b7357f59c9df594a5084cd623b2779b6519
abdulsalamamtech <abdulsalamamtech@gmail.com>
2024-11-07 11:47:02 +0100

    Add api store project reguest

---
+++ app/Http/Requests/Api/StoreProjectRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'banner' => ['required','image','mimes:jpeg,png,jpg,gif,svg|max:2048'],
+            'title' => ['required','string'],
+            'description' => ['required','string'],
+            'tags' => ['required','string'],
+            'category' => ['required','string'],
+            'status' => ['required','string'],
+            // 'approved_by' => ['required','string'],
+            // 'views' => ['required','integer'],
+            // 'likes' => ['required','integer'],
+            // 'shares' => ['required','integer'],
+            // 'deleted_by' => ['required','integer']
+        ];
+    }
+}
