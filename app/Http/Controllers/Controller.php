b39eaea2019c94cc74403feacea49c3d173dbb60
abdulsalamamtech <abdulsalamamtech@gmail.com>
2024-11-14 17:44:19 +0100

    Modified http controller file

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,8 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Traits\ApiResponseTrait;
+use App\Traits\UploadFileTrait;
 
 abstract class Controller
 {
-    use ApiResponseTrait;
+    use ApiResponseTrait, UploadFileTrait;
 }
