e7e5ebb16522297f90dc307cbc1b0d3faa8ce875
abdulsalamamtech <abdulsalamamtech@gmail.com>
2024-11-16 08:24:16 +0100

    Modified  upload file trait

--- app/Traits/UploadFileTrait.php
+++ app/Traits/UploadFileTrait.php
@@ -8,17 +8,17 @@ trait UploadFileTrait
 
 {
 
-    // upload image
-    public function uploadImage($request)
+    // upload file
+    public function uploadImage($request, $fileName = 'image')
     {
 
         // Save the file to disk
-        $path = $request->file('banner')->store('images', 'public');
+        $path = $request->file($fileName)->store('images', 'public');
 
         // Get the public URL for accessing the uploaded file
         $url = Storage::url($path);
 
-        $file = $request->file('banner');
+        $file = $request->file($fileName);
         $originName = $file->getClientOriginalName();
         $originExt = $file->extension();
 
@@ -32,13 +32,53 @@ trait UploadFileTrait
 
         return [
             'path' => $url,
-            'name' => $originName,
-            'ext' => $originExt,
-            'size' => $file->getSize(),
+            'original_name' => $originName,
             'type' => $file->getMimeType(),
-            'url' => url($url)
+            'name' => $fileName,
+            // 'file_id' => $file->get,
+            'url' => url($url),
+            'size' => $file->getSize(),
+            'hosted_at' => 'directory', // cloudinary | imagekit
+            // 'ext' => $originExt,
         ];
 
     }
-    
+
+
+    // Update file
+    public function updateImage($request, $fileName = 'image')
+    {
+
+        // Save the file to disk
+        $path = $request->file($fileName)->store('images', 'public');
+
+        // Get the public URL for accessing the uploaded file
+        $url = Storage::url($path);
+
+        $file = $request->file($fileName);
+        $originName = $file->getClientOriginalName();
+        $originExt = $file->extension();
+
+        // $fileName = time() . '.' . $originExt;
+        // $file->storeAs('public/images/', $fileName);
+        // $upload = $file->move(public_path('assets'), $fileName);
+        // $file->storeAs('public/assets', $fileName);
+        // return asset('public/assets/'. $fileName);
+        // $request->file('banner')->storeAs('assets', $fileName);
+
+
+        return [
+            'path' => $url,
+            'original_name' => $originName,
+            'type' => $file->getMimeType(),
+            'name' => $fileName,
+            // 'file_id' => $file->get,
+            'url' => url($url),
+            'size' => $file->getSize(),
+            'hosted_at' => 'directory', // cloudinary | imagekit
+            // 'ext' => $originExt,
+        ];
+
+    }
+
 }
