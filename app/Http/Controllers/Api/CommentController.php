d0547448ae56acc92fdd1188d8870244f58a9234
abdulsalamamtech <abdulsalamamtech@gmail.com>
2024-11-16 08:26:12 +0100

    Modified api comment controller

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\StoreCommentRequest;
+use App\Http\Requests\Api\UpdateCommentRequest;
+use App\Models\Api\Comment;
 use App\Models\Api\Project;
 use Illuminate\Http\Request;
 
@@ -13,34 +16,61 @@ class CommentController extends Controller
      */
     public function index(Project $project)
     {
-        $project->load(['banner', 'comments']);
-        return $project;
+        $comments = $project->comments;
+
+        if (!$comments) {
+            return $this->sendError([], 'unable to load comments', 500);
+        }
+
+        return $this->sendSuccess($comments, 'successful', 201);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Project $project, StoreCommentRequest $request)
     {
-        //
+        $user = $request->user();
+        $data = $request->validated();
+        $data['user_id'] = $user->id;
+        $data['project_id'] = $project->id;
+
+        $comment = $project->comments()->create($data);
+
+        if (!$comment) {
+            return $this->sendError([], 'unable to create comment', 500);
+        }
+
+        return $this->sendSuccess($comment, 'comment created', 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show($project, $comment)
+    public function show(Project $project, Comment $comment)
     {
-        //
-        $data = [$project, $comment];
-        return response()->json(['data'=> $data, 'message' => 'Comments are not supported in this version'], 404);
+        $comment = Comment::where('project_id', $project->id)->find($comment);
+        if (!$comment) {
+            return $this->sendError([], 'comment not found', 404);
+        }
+
+        return $this->sendSuccess($comment, 'successful', 201);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Project $project, UpdateCommentRequest $request)
     {
-        //
+        return $request;
+        $data = $request->validated();
+        $comment = $project->comments()->update($data);
+
+        if (!$comment) {
+            return $this->sendError([], 'unable to update comment', 500);
+        }
+
+        return $this->sendSuccess($comment, 'comment updated', 201);
     }
 
     /**
