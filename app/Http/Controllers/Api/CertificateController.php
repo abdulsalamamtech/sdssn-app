0bd1c8013a91b8919091964bdbc7ec1a30b56887
abdulsalamamtech <abdulsalamamtech@gmail.com>
2024-11-16 08:26:34 +0100

    Modified api certificate controller

--- app/Http/Controllers/Api/CertificateController.php
+++ app/Http/Controllers/Api/CertificateController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\StoreCertificateRequest;
+use App\Http\Requests\Api\UpdateCertificateRequest;
+use App\Models\Api\Certificate;
+use App\Models\Assets;
 use Illuminate\Http\Request;
 
 class CertificateController extends Controller
@@ -12,30 +16,55 @@ class CertificateController extends Controller
      */
     public function index()
     {
-        //
+        // Get all certificate with their associated relationships
+        $certificates = Certificate::with(['user', 'addedBy'])->get();
+
+        if (!$certificates) {
+            return $this->sendError([], 'unable to load certificates', 500);
+        }
+
+        return $this->sendSuccess($certificates, 'successful', 200);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreCertificateRequest $request)
     {
-        //
+        $user = $request->user();
+        $data = $request->validated();
+        $data['added_by'] = $user->id;
+
+        // Upload data
+        $upload =  $this->uploadImage($request, 'certificate');
+
+        // Add assets
+        $asset = Assets::create($upload);
+        $data['asset_id'] = $asset->id;
+
+        // Add certificate
+        $certificate = Certificate::create($data);
+        $certificate->load(['user', 'added_by']);
+
+        if (!$certificate) {
+            return $this->sendError([], 'unable to create certificate', 500);
+        }
+
+        return $this->sendSuccess($certificate, 'certificate created', 201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Request $request)
+    public function show(Certificate $certificate)
     {
-        $user = $request->user();
-        $user->load(['social']);
+        $certificate->load(['addedBy']);
 
-        if(!$user){
-            return $this->sendError([], 'unable to load user profile', 500);
+        if(!$certificate){
+            return $this->sendError([], 'certificate not found', 404);
         }
 
-        return $this->sendSuccess($user, 'successful', 200);
+        return $this->sendSuccess($certificate, 'successful', 200);
 
 
     }
@@ -43,23 +72,34 @@ class CertificateController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    // public function update(UpdateUserProfileRequest $request)
-    // {
+    public function update(Certificate $certificate, UpdateCertificateRequest $request)
+    {
 
-    //     // return "update profile";
-    //     $data = $request->validated();
-    //     $user = $request->user();
-    //     $user->update($data);
-    //     $user->load(['social']);
+        $user = $request->user();
+        $data = $request->validated();
+        $data['added_by'] = $user->id;
 
-    //     $user_profile = $user;
+        if($request->certificate){
 
-    //     if(!$user_profile){
-    //         return $this->sendError([], 'unable to update profile', 500);
-    //     }
+            // // Upload data
+            // $upload =  $this->uploadImage($request, 'certificate');
 
-    //     return $this->sendSuccess($user_profile, 'profile updated', 201);
-    // }
+            // // Add assets
+            // $asset = Assets::create($upload);
+            // // $a = $certificate->
+            // $data['asset_id'] = $asset->id;
+        }
+
+        // Add certificate
+        $certificate->update($data);
+        $certificate->load(['user', 'addedBy']);
+
+        if (!$certificate) {
+            return $this->sendError([], 'unable to update certificate', 500);
+        }
+
+        return $this->sendSuccess($certificate, 'certificate updated', 201);
+    }
 
     /**
      * Remove the specified resource from storage.
